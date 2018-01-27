1e170d67edab9f37021eaaae71ca8e8ef957229c
Login Jones <login.jones@gmail.com>
2018-01-27 01:09:46 -0500

    first commit

---
+++ src/main/java/com/rbc/cloud/adoption/apollo/validate/apollovalidator/ApolloValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.rbc.cloud.adoption.apollo.validate.apollovalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloValidatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rbc/cloud/adoption/apollo/validate/apollovalidator/ApolloValidatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbc.cloud.adoption.apollo.validate.apollovalidator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApolloValidatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
