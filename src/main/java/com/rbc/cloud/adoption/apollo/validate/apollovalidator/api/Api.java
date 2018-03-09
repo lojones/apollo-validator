08d43bc16fd39273ccd5a696ec9dca6fe2dcd305
Login Jones <login.jones@gmail.com>
2018-03-08 23:16:12 -0500

    added api to talk to jira

---
+++ src/main/java/com/rbc/cloud/adoption/apollo/validate/apollovalidator/api/Api.java
@@ -0,0 +1,10 @@
+package com.rbc.cloud.adoption.apollo.validate.apollovalidator.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Api {
+
+    @RequestMapping("/api/validate")
+}
